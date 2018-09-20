5357e99cca15a03ee6e6ffbcdfd6b8b94fd3eb38
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2018-09-19 21:31:47 -0500

    increase timeouts

--- generators/app/templates/node/webpack.config.js
+++ generators/app/templates/node/webpack.config.js
@@ -2,7 +2,7 @@ const slsw = require('serverless-webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: Object.assign({ server: './server.js' }, slsw.lib.entries),
   target: 'node',
   externals: [nodeExternals()]
