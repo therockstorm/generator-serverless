712f7240bb93d5a14278256a3b974946b4572c73
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2018-12-12 20:35:10 -0600

    upgrade deps

--- generators/app/templates/typeScript/src/handler.ts
+++ generators/app/templates/typeScript/src/handler.ts
@@ -1,14 +1,13 @@
 import { log } from '@therockstorm/utils'
 import {
   Context,
-  APIGatewayProxyHandler,
   APIGatewayProxyEvent,
   APIGatewayProxyResult
 } from 'aws-lambda'
 
-export const handle: APIGatewayProxyHandler = async (
-  evt: APIGatewayProxyEvent,
-  ctx: Context
+export const handle = async (
+  evt: APIGatewayProxyEvent | {},
+  ctx: Context | {}
 ): Promise<APIGatewayProxyResult> => {
   log(JSON.stringify(evt), JSON.stringify(ctx))
   return { statusCode: 200, body: JSON.stringify(evt) }
--- generators/app/templates/typeScript/test/handler.test.ts
+++ generators/app/templates/typeScript/test/handler.test.ts
@@ -1,8 +1,8 @@
 import { handle } from '../src/handler'
 
 test('handler', async () => {
-  const evt = { key: 'value' }
+  const evt = { httpMethod: 'GET' }
 
   const res = await handle(evt, {})
-  expect(res).toEqual(res({ statusCode: 200, body: JSON.stringify(evt) }))
+  expect(res).toEqual({ statusCode: 200, body: JSON.stringify(evt) })
 })
