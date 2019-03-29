cc994fb6577f5aa45f2d63059cbbe019d3dfd731
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2019-03-29 09:50:19 -0500

    fix TypeScript test

--- generators/app/templates/typeScript/test/handler.test.ts
+++ generators/app/templates/typeScript/test/handler.test.ts
@@ -1,8 +1,9 @@
+import { APIGatewayProxyEvent, Callback, Context } from 'aws-lambda'
 import { handle } from '../src/handler'
 
 test('handler', async () => {
-  const evt = { httpMethod: 'GET' } as any
+  const evt = { httpMethod: 'GET' } as APIGatewayProxyEvent
 
-  const res = await handle(evt)
+  const res = await handle(evt, {} as Context, {} as Callback)
   expect(res).toEqual({ statusCode: 200, body: JSON.stringify(evt) })
 })
