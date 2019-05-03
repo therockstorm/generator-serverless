92a452865d76fe27cdb4ff87048bd17d1c8fc681
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2019-05-03 09:05:27 -0500

    update TypeScript deps

--- generators/app/templates/typeScript/src/handler.ts
+++ generators/app/templates/typeScript/src/handler.ts
@@ -1,7 +1,9 @@
-import { log } from '@therockstorm/utils'
-import 'source-map-support/register'
+import { log } from "@therockstorm/utils"
+import "source-map-support/register"
 
-export const handle = async (evt: any): Promise<any> => {
+export const handle = async (
+  evt: any
+): Promise<{ statusCode: number; body: string }> => {
   log(JSON.stringify(evt))
   return { statusCode: 200, body: JSON.stringify(evt) }
 }
--- generators/app/templates/typeScript/test/handler.test.ts
+++ generators/app/templates/typeScript/test/handler.test.ts
@@ -1,7 +1,7 @@
-import { handle } from '../src/handler'
+import { handle } from "../src/handler"
 
-test('handler', async () => {
-  const evt = { httpMethod: 'GET' }
+test("handler", async () => {
+  const evt = { httpMethod: "GET" }
 
   const res = await handle(evt)
   expect(res).toEqual({ statusCode: 200, body: JSON.stringify(evt) })
